a1ff2a9fde262367c4fbfe8d954a7a220093d86f
FahdAzmy <azmyfahd66@gmail.com>
2024-09-22 20:17:00 +0200

    Make the Login Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,14 @@
+import Login from "./pages/login/Login";
+
 function App() {
-  return <></>;
+  return (
+    <div className=" bg-white">
+      <h1 className="font-bold text-2xl bg-white  text-amber-800  ">Talkie</h1>
+      <div className=" bg-white w-full h-screen flex items-center justify-center">
+        <Login />
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "custom-blue": "#C0DBEA",
+      },
+    },
   },
-  plugins: [],
-}
 
+  // eslint-disable-next-line no-undef
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["light", "dark", "cupcake"],
+  },
+};
